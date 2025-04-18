f19a56573a7471f0123c3aea11a8c4b3c0ff162f
NguyenHuuVanh <vietbui673@gmail.com>
2025-04-18 21:57:34 +0700

    update: responsive footer page show in tablet and mobile

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import classNames from "classnames/bind";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 import styles from "./Footer.module.scss";
+
 import {faFacebook, faInstagram, faThreads} from "@fortawesome/free-brands-svg-icons";
+import {Col, Container, Row} from "react-bootstrap";
 
 const cx = classNames.bind(styles);
 
@@ -10,52 +13,64 @@ const Footer = () => {
   return (
     <footer className={cx("footer")}>
       <div className={cx("footer-content")}>
-        <div className={cx("footer-section")}>
-          <h4 className={cx("section-title")}>INFO</h4>
-          <ul className={cx("section-list")}>
-            <li>Formats</li>
-            <li>Compression</li>
-            <li>Pricing</li>
-            <li>FAQ</li>
-            <li>Status</li>
-          </ul>
-        </div>
+        <Container className={cx("footer-container")}>
+          <Row className={cx("footer-section")}>
+            <Col xs={6} sm={6} lg={3}>
+              <div className={cx("footer-section")}>
+                <h4 className={cx("section-title")}>INFO</h4>
+                <ul className={cx("section-list")}>
+                  <li>Formats</li>
+                  <li>Compression</li>
+                  <li>Pricing</li>
+                  <li>FAQ</li>
+                  <li>Status</li>
+                </ul>
+              </div>
+            </Col>
 
-        <div className={cx("footer-section")}>
-          <h4 className={cx("section-title")}>RESOURCES</h4>
-          <ul className={cx("section-list")}>
-            <li>Developer API</li>
-            <li>Tools</li>
-            <li>Blog</li>
-          </ul>
-        </div>
+            <Col xs={6} sm={6} lg={3}>
+              <div className={cx("footer-section")}>
+                <h4 className={cx("section-title")}>RESOURCES</h4>
+                <ul className={cx("section-list")}>
+                  <li>Developer API</li>
+                  <li>Tools</li>
+                  <li>Blog</li>
+                </ul>
+              </div>
+            </Col>
 
-        <div className={cx("footer-section")}>
-          <h4 className={cx("section-title")}>COMPANY</h4>
-          <ul className={cx("section-list")}>
-            <li>About Us</li>
-            <li>Sustainability</li>
-            <li>Terms of Service</li>
-            <li>Privacy</li>
-          </ul>
-        </div>
+            <Col xs={6} sm={6} lg={3}>
+              <div className={cx("footer-section")}>
+                <h4 className={cx("section-title")}>COMPANY</h4>
+                <ul className={cx("section-list")}>
+                  <li>About Us</li>
+                  <li>Sustainability</li>
+                  <li>Terms of Service</li>
+                  <li>Privacy</li>
+                </ul>
+              </div>
+            </Col>
 
-        <div className={cx("newsletter-section")}>
-          <h4 className={cx("section-title")}>Subscribe to our email newsletter</h4>
-          <div className={cx("newsletter-form")}>
-            <input type="email" placeholder="Your email" />
-            <button className={cx("subscribe-btn")}>SUBSCRIBE</button>
-          </div>
-          <div className={cx("follow-us")}>
-            <span>Follow us:</span>
-            <div className={cx("social-icons")}>
-              <FontAwesomeIcon icon={faFacebook} />
-              <FontAwesomeIcon icon={faInstagram} />
-              <FontAwesomeIcon icon={faThreads} />
-              {/* <FontAwesomeIcon icon={["fa-brands", "fa-threads"]} /> */}
-            </div>
-          </div>
-        </div>
+            <Col xs={6} sm={6} lg={3}>
+              <div className={cx("newsletter-section")}>
+                <h4 className={cx("section-title")}>Subscribe to our email newsletter</h4>
+                <div className={cx("newsletter-form")}>
+                  <input type="email" placeholder="Your email" />
+                  <button className={cx("subscribe-btn")}>SUBSCRIBE</button>
+                </div>
+                <div className={cx("follow-us")}>
+                  <span>Follow us:</span>
+                  <div className={cx("social-icons")}>
+                    <FontAwesomeIcon icon={faFacebook} />
+                    <FontAwesomeIcon icon={faInstagram} />
+                    <FontAwesomeIcon icon={faThreads} />
+                    {/* <FontAwesomeIcon icon={["fa-brands", "fa-threads"]} /> */}
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </footer>
   );
